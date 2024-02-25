6e3c997655e5235fd91cc9c3784e11dceda3412f
Anuja Nimesh <anujanimesh@gmail.com>
2024-02-25 20:15:38 +0530

    feat:layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { page } from '$app/stores';
+
+	$: pathname = $page.url.pathname;
+	const links = ['Shop', 'Contact Us', 'About'];
+</script>
+
+<header>
+	<nav>
+		<a href="/"><img src="/logo.svg" alt="logo" /></a>
+		<div>
+			{#each links as link}
+				{@const href = '/' + link.toLowerCase().replaceAll(' ', '-')}
+				<a {href} class={pathname === href ? 'active' : ''}>{link}</a>
+			{/each}
+		</div>
+	</nav>
+</header>
+<div class="spacing"></div>
+<slot />
+
+<style lang="scss">
+	header {
+		> nav {
+			@apply fixed flex items-center justify-between;
+			@apply z-50 h-14 w-full px-12;
+			@apply bg-white/60  backdrop-blur-sm;
+			@apply border-y border-border/40 border-t-transparent;
+			> a > img {
+				@apply h-8;
+			}
+			> div {
+				@apply flex gap-8;
+				> a {
+					@apply text-sm opacity-60 hover:opacity-80;
+					&.active {
+						@apply opacity-100;
+					}
+				}
+			}
+		}
+	}
+	.spacing {
+		@apply h-14;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,27 @@
 	import '../app.pcss';
 </script>
 
-<slot />
+<svelte:head>
+	<title>E Commerce</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+		rel="stylesheet"
+	/>
+	<link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	* {
+		font-family: 'Inter';
+	}
+
+	main {
+		@apply h-svh bg-slate-50;
+	}
+</style>
