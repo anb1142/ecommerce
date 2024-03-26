a7afc922d0d6e19392acdd50ae47fa54be9769cc
Anuja Nimesh <anujanimesh@gmail.com>
2024-03-26 23:25:50 +0530

    fix:sidebar

--- src/lib/components/Sidebar/Sidebar.svelte
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -6,30 +6,21 @@
 </script>
 
 <aside>
-	<div>
-		{#each Object.entries(links) as [link, { text }]}
-			{@const href = '/admin/' + link}
-			{@const active = pathname === href ? 'active' : ''}
-			<a {href} class:active>{active ? '> ' : ''}{text}</a>
-		{/each}
-		<a href="/admin/logout">Logout</a>
-	</div>
+	{#each Object.entries(links) as [link, { text }]}
+		{@const href = '/admin/' + link}
+		{@const active = pathname === href ? 'active' : ''}
+		<a {href} class:active>{active ? '> ' : ''}{text}</a>
+	{/each}
+	<a href="/admin/logout">Logout</a>
 </aside>
 
 <style lang="scss">
 	aside {
-		@apply flex flex-col p-4;
-		@apply border  bg-white;
-		width: 20%;
-		height: 100svh;
-
-		> div {
-			@apply flex flex-col gap-4;
-			> a {
-				@apply opacity-80 hover:opacity-100;
-				&.active {
-					@apply opacity-100;
-				}
+		@apply flex  w-1/5   flex-col gap-4 border-r p-4;
+		> a {
+			@apply opacity-80 hover:opacity-100;
+			&.active {
+				@apply opacity-100;
 			}
 		}
 	}
