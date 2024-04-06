17b5b9cf170e91fdcb714fd025aa12305faddfe3
Anuja Nimesh <anujanimesh@gmail.com>
2024-04-06 19:23:27 +0530

    feat(SubmitForm): file uploading

--- src/lib/components/SubmitForm.svelte
+++ src/lib/components/SubmitForm.svelte
@@ -2,9 +2,16 @@
 	import { enhance } from '$app/forms';
 	import { Button } from '@/components/ui/button';
 	export let btn = 'Submit';
+	export let inclFile = false;
 </script>
 
-<form method="POST" class="gap-4" {...$$props} use:enhance>
+<form
+	method="POST"
+	class="gap-4"
+	{...$$props}
+	enctype={inclFile ? 'multipart/form-data' : 'application/x-www-form-urlencoded'}
+	use:enhance
+>
 	<slot />
 	<Button type="submit">{btn}</Button>
 </form>
