d7bf8c97117cb2406649f84835c7842ef6051434
Anuja Nimesh <anujanimesh@gmail.com>
2024-04-06 19:22:32 +0530

    fix(extractFormValues): fixed types for boolean fields

--- src/lib/utils/extractFormValues.ts
+++ src/lib/utils/extractFormValues.ts
@@ -4,16 +4,22 @@ const extractFormValues: <const TFields extends IFields>(
 	fields: TFields,
 	formData: FormData
 ) => {
-	[Key in TFields[number]['name']]: Extract<
-		TFields[number],
-		{ name: Key }
-	>['valType'] extends 'number'
-		? number
-		: string;
+	[Key in TFields[number]['name']]: Extract<TFields[number], { name: Key }>['type'] extends 'file'
+		? File
+		: Extract<TFields[number], { name: Key }>['type'] extends 'boolean'
+			? boolean
+			: Extract<TFields[number], { name: Key }>['valType'] extends 'number'
+				? number
+				: string;
 } = (fields, formData: FormData) => {
 	const data: any = {};
 	for (const field of fields) {
+		if (field.type === 'file') {
+			data[field.name] = formData.get(field.name) as File;
+			continue;
+		}
 		const value = formData.get(field.name) as string;
+		if (field.type === 'boolean') data[field.name] = value === 'true' ? true : false;
 		if (field.valType === 'number') data[field.name] = Number(value);
 		else data[field.name] = value;
 	}
