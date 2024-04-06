8cbd13759d803578fbe19a98bd1e6dd603116b8c
Anuja Nimesh <anujanimesh@gmail.com>
2024-04-06 19:28:14 +0530

    feat:product table

--- src/routes/admin/products/new/+page.server.ts
+++ src/routes/admin/products/new/+page.server.ts
@@ -4,38 +4,28 @@ import { category, product } from '@/server/db/schema';
 import { extractFormValues } from '@/utils/extractFormValues';
 import type { Actions } from '@sveltejs/kit';
 
-export const load = async () => {
+const getFields = async () => {
 	const getCategoriesForSelect = async () =>
 		await db.select({ value: category.id, label: category.name }).from(category);
 
 	const fields = [
-		...productFields,
 		{
 			name: 'category_id',
 			label: 'Category',
 			valType: 'number',
 			opts: await getCategoriesForSelect()
-		}
+		},
+		...productFields
 	] as const;
-
-	return { fields };
+	return fields;
+};
+export const load = async () => {
+	return { fields: await getFields() };
 };
 
 export const actions = {
 	create: async ({ request }) => {
-		const getCategoriesForSelect = async () =>
-			await db.select({ value: category.id, text: category.name }).from(category);
-
-		const fields = [
-			...productFields,
-			{
-				name: 'category_id',
-				label: 'Category',
-				valType: 'number',
-				opts: await getCategoriesForSelect()
-			}
-		] as const;
-
+		const fields = await getFields();
 		const formData = await request.formData();
 		const data = extractFormValues(fields, formData);
 		await db.insert(product).values(data);
