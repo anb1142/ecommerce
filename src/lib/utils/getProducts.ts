c3e9e1a9eb09143a217b4fdc7c8a404a50c3aa7d
Anuja Nimesh <anujanimesh@gmail.com>
2024-04-21 18:52:02 +0530

    fix:getImagedProduct promise.all

--- src/lib/utils/getProducts.ts
+++ src/lib/utils/getProducts.ts
@@ -5,12 +5,14 @@ export const getImagedProduct = async (product: IImagedProduct) => {
 	const productImages = await db.query.product_image.findMany({
 		where: (product_image, { eq }) => eq(product_image.product_id, product.id)
 	});
-	product.images = [];
-	for (const productImage of productImages) {
-		const res = await db.query.image.findMany({
-			where: (image, { eq }) => eq(image.id, productImage.image_id)
-		});
-		product.images.push(res[0]);
-	}
+	product.images = await Promise.all(
+		productImages.map(async (productImage) => {
+			const res = await db.query.image.findMany({
+				where: (image, { eq }) => eq(image.id, productImage.image_id)
+			});
+			return res[0];
+		})
+	);
+
 	return product;
 };
